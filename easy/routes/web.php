b2b3b1209147390e8e2691651693ee11de97ed8c
Plugin Mike <michael.sauerman@synthesis.co.za>
2024-07-16 11:32:35 +0200

    feat(easy): added item model

--- easy/routes/web.php
+++ easy/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('items', ItemController::class);
 
 Route::get('/', function () {
     return view('welcome');
