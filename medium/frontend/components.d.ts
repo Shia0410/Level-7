7f4388652fba34247fd697222f26e34d4f0ebd77
Plugin Mike <michael.sauerman@synthesis.co.za>
2024-07-17 12:29:49 +0200

    Project Finilization

--- medium/frontend/components.d.ts
+++ medium/frontend/components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     ItemManager: typeof import('./src/components/ItemManager.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
